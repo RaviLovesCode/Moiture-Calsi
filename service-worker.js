3fbe8f16961af09263d133156101f5b005afd5ad
RaviLovesCode <72981534+RaviLovesCode@users.noreply.github.com>
2025-08-23 00:26:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("moisture-calc-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
